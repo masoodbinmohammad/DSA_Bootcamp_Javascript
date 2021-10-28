351b662c2dc2db0f17ef357461bb1c169e54ef21
Masood Mohammad <45809678+masoodbinmohammad@users.noreply.github.com>
2021-10-28 10:32:15 +0530

    Create Patterns.js

---
+++ Lectures/Patterns.js
@@ -0,0 +1,14 @@
+# Pattern 31
+
+function pattern31(n) {
+        let originalN = n;
+        n = 2 * n;
+        for (let row = 0; row <= n; row++) {
+            for (let col = 0; col <= n; col++) {
+                let atEveryIndex = originalN - Math.min(Math.min(row, col), Math.min(n - row, n - col));
+                process.stdout.write(atEveryIndex + " ");
+            }
+            console.log('\n');
+        }
+    }
+pattern31(4);
