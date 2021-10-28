0cd51a9d0176035d7ff9a076d6589c3709b280d1
Masood Mohammad <45809678+masoodbinmohammad@users.noreply.github.com>
2021-10-28 11:08:32 +0530

    Update Patterns.js

--- Lectures/Patterns.js
+++ Lectures/Patterns.js
@@ -1,12 +1,48 @@
 function pattern31(n) {
-        let originalN = n;
+        const originalN = n;
         n = 2 * n;
         for (let row = 0; row <= n; row++) {
             for (let col = 0; col <= n; col++) {
-                let atEveryIndex = originalN - Math.min(Math.min(row, col), Math.min(n - row, n - col));
+                const atEveryIndex = originalN - Math.min(Math.min(row, col), Math.min(n - row, n - col));
                 process.stdout.write(atEveryIndex + " ");
             }
             console.log('\n');
         }
     }
-pattern31(4);
+
+function pattern30(n){
+   for (let row = 1; row <= n; row++) {
+            for (let space = 0; space < n-row; space++) {
+                process.stdout.write("  ");
+            }
+            for (let col = row; col >= 1; col--) {
+                process.stdout.write(col + " ");
+            }
+            for (let col = 2; col <= row; col++) {
+                process.stdout.write(col + " ");
+            }
+           console.log('\n');
+        }
+}
+
+function pattern17(n){
+   for (let row = 1; row <= 2 * n; row++) {
+            let c = row > n ? 2 * n - row: row;
+
+            for (let space = 0; space < n-c; space++) {
+                process.stdout.write("  ");
+            }
+
+            for (let col = c; col >= 1; col--) {
+                process.stdout.write(col + " ");
+            }
+            for (let col = 2; col <= c; col++) {
+                process.stdout.write(col + " ");
+            }
+            console.log('\n');
+        }
+}
+
+//pattern31(4);
+//pattern30(10);
+pattern17(3)
