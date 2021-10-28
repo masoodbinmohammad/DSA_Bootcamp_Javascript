e9d190ce6a3e76c6d707f5575bf8eeb2c9589d47
Masood Mohammad <45809678+masoodbinmohammad@users.noreply.github.com>
2021-10-28 11:49:02 +0530

    Update Patterns.js

--- Lectures/Patterns.js
+++ Lectures/Patterns.js
@@ -1,27 +1,58 @@
-function pattern31(n) {
-        const originalN = n;
-        n = 2 * n;
-        for (let row = 0; row <= n; row++) {
-            for (let col = 0; col <= n; col++) {
-                const atEveryIndex = originalN - Math.min(Math.min(row, col), Math.min(n - row, n - col));
-                process.stdout.write(atEveryIndex + " ");
+
+function pattern1(n){
+   for (let row = 1; row <= n; row++) {
+            // for every row, run the col
+            for (let col = 1; col <= n; col++) {
+                process.stdout.write("* ");
             }
+            // when one row is prleted, we need to add a newline
             console.log();
         }
-    }
+}
 
-function pattern30(n){
-   for (let row = 1; row <= n; row++) {
-            for (let space = 0; space < n-row; space++) {
-                process.stdout.write("  ");
+function pattern2(n){
+  for (let row = 1; row <= n; row++) {
+            // for every row, run the col
+            for (let col = 1; col <= row; col++) {
+                process.stdout.write("* ");
             }
-            for (let col = row; col >= 1; col--) {
+            // when one row is prleted, we need to add a newline
+            console.log();
+        }
+}
+
+function pattern3(n){
+  for (let row = 1; row <= n; row++) {
+            // for every row, run the col
+            //for (let col = 1; col <= n-row+1; col++) {
+              //  process.stdout.write("* ");
+            //}
+            for (let col = n-row+1; col >=1 ; col--) {
+                process.stdout.write("* ");
+            }
+            // when one row is prleted, we need to add a newline
+            console.log();
+        }
+}
+
+function pattern4(n){
+  for (let row = 1; row <= n; row++) {
+            // for every row, run the col
+            for (let col = 1; col <= row; col++) {
                 process.stdout.write(col + " ");
             }
-            for (let col = 2; col <= row; col++) {
-                process.stdout.write(col + " ");
+            // when one row is prleted, we need to add a newline
+            console.log();
+        }
+}
+
+function pattern5(n){
+  for (let row = 1; row < 2 * n; row++) {
+            let totalColsInRow = row > n ? 2 * n - row: row;
+            for (let col = 0; col < totalColsInRow; col++) {
+               process.stdout.write("* ");
             }
-           console.log();
+            console.log();
         }
 }
 
@@ -43,6 +74,7 @@ function pattern17(n){
         }
 }
 
+
 function pattern28(n){
   for (let row = 1; row < 2 * n; row++) {
             const totalColsInRow = row > n ? 2 * n - row: row;
@@ -59,7 +91,40 @@ function pattern28(n){
         }
 }
 
+function pattern30(n){
+   for (let row = 1; row <= n; row++) {
+            for (let space = 0; space < n-row; space++) {
+                process.stdout.write("  ");
+            }
+            for (let col = row; col >= 1; col--) {
+                process.stdout.write(col + " ");
+            }
+            for (let col = 2; col <= row; col++) {
+                process.stdout.write(col + " ");
+            }
+           console.log();
+        }
+}
+
+function pattern31(n) {
+        const originalN = n;
+        n = 2 * n;
+        for (let row = 0; row <= n; row++) {
+            for (let col = 0; col <= n; col++) {
+                const atEveryIndex = originalN - Math.min(Math.min(row, col), Math.min(n - row, n - col));
+                process.stdout.write(atEveryIndex + " ");
+            }
+            console.log();
+        }
+    }
+
+
 //pattern31(4);
 //pattern30(10);
 //pattern17(3)
-pattern28(4)
+//pattern28(4)
+//pattern5(4)
+//pattern4(4)
+//pattern3(4)
+//pattern1(4)
+//pattern2(4)
