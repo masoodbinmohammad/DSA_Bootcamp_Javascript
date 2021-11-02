dd5e7023a040d6f5498191a0eed8cf4531d63d40
Masood Mohammad <45809678+masoodbinmohammad@users.noreply.github.com>
2021-11-02 13:01:58 +0530

    remove dice solution from phonepad solution file

--- Lectures/Recursion/phonepad.js
+++ Lectures/Recursion/phonepad.js
@@ -1,31 +1,3 @@
-function dice(p, target){
-   if (target == 0) {
-            console.log(p);
-            return;
-        }
-
-        for (let i = 1; i <= 6 && i <= target; i++) {
-            dice(p + i, target - i);
-        }
-}
-let arrAll = []
-function diceReturn(p, target){
-  if(target == 0){
-    let arr = [];
-    arr.push(p)
-    return arr;
-  }
-  for(let i =1 ; i < 6 && i <= target; i++){
-    const ans = diceReturn(p+i, target -1);
-    arrAll.push(ans);
-  }
-  return arrAll
-}
-
-//dice('', 4);
-//diceReturn('', 4)
-
-
 function phonePad(p, up){
   if (up.length == 0) {
             console.log(p);
